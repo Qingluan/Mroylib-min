e79ddcf9f33ad2c4c1a3a572927460e2a351df89
dr <dr@users-MacBook-Pro.local>
2019-03-06 11:49:15 +0800

    ready fix elasticserach

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='mroylib_min',
-    version='2.0.5',
+    version='2.0.7',
     description='some libs',
     url='https://github.com/Qingluan/.git',
     author='Qing luan',
